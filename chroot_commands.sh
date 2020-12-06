4cfc1e45b919f6a19a282845b408a274b54c3957
Cat-Gaming <47046268+Cat-Gaming@users.noreply.github.com>
2020-12-05 21:57:38 -0500

    Update chroot_commands.sh

--- chroot_commands.sh
+++ chroot_commands.sh
@@ -27,13 +27,59 @@ passwd
 # install bootloader
 pacman -S grub efibootmgr
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+# make grub config
+grub-mkconfig -o /boot/grub/grub.cfg
+# install microcode
+pacman -S amd-ucode # amd only
+# pacman -S intel-ucode # intel only
 
 # install sudo
-pacman -S sudo
+pacman -S sudo vim
 useradd -m tyler # replace tyler with your username
 usermod -aG wheel tyler # add user to wheel
-echo "Allow the group Wheel in by uncommenting the line that says: "
 
-# install extra stuff
+# change sudoers file
+echo "Change the Sudoers file to your needs!"
+sleep 4
+EDITOR=vim visudo
+
+chown tyler:tyler /home/tyler # makes {username} own the directory
+mkdir /home/tyler # change this to your username
+cd /home/tyler # change this to your username
+
+# remove vim
+pacman -R vim
+
+# install wget
+pacman -S wget
+
+# install extra stuff and vim because i like it
+pacman -S firefox terminator visual-studio-code-bin vim # my extra stuff you dont need this if you dont want it
+
+# make my directories
+mkdir code
+mkdir code/godot
+mkdir code/asm
+mkdir code/repos
+
+# download Godot 3.2.3
+wget https://downloads.tuxfamily.org/godotengine/3.2.3/Godot_v3.2.3-stable_x11.64.zip
+unzip Godot_v3.2.3-stable_x11.64.zip
+mv Godot_v3.2.3-stable_x11.64 Godot.64
+rm Godot_v3.2.3-stable_x11.64.zip
 
 
+# install deps needed to build yay and other packages
+pacman -S base-devel
+
+# install git
+pacman -S git
+# install yay
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd ..
+rm -r yay
+
+echo Done!
+sleep 2
