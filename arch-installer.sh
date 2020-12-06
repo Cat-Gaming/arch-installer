926482b12ab152ed93467859396e8bc2e491fbab
Cat-Gaming <47046268+Cat-Gaming@users.noreply.github.com>
2020-12-05 20:46:46 -0500

    Update arch-installer.sh

--- arch-installer.sh
+++ arch-installer.sh
@@ -3,6 +3,12 @@
 if [[ $(ls /sys/firmware/efi/efivars) ]]; then
     echo "Booted in UEFI Mode."
 else
-    echo "Error Not Booted in UEFI Mode"
+    echo "Error Not Booted in UEFI Mode Please boot in UEFI Mode!"
     read -p "Error" input
+fi
+
+if [[ $( ping -c 2 archlinux.org ) ]]; then
+    echo "Connected to the internet!"
+else
+    echo "Not connected to the internet!"
 fi
